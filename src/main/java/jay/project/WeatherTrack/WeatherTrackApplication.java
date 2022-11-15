f9d69d7198178cf3f4f1dd8406b659364e707a30
leow998 <leow998@gmail.com>
2022-11-15 22:31:54 +0800

    CORS

--- src/main/java/jay/project/WeatherTrack/WeatherTrackApplication.java
+++ src/main/java/jay/project/WeatherTrack/WeatherTrackApplication.java
@@ -3,6 +3,10 @@ package jay.project.WeatherTrack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class WeatherTrackApplication {
 
@@ -10,4 +14,14 @@ public class WeatherTrackApplication {
 		SpringApplication.run(WeatherTrackApplication.class, args);
 	}
 
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                        .allowedOrigins("https://weathertrack-angular.herokuapp.com", "http://localhost:4200");
+			}
+		};
+	}
 }
